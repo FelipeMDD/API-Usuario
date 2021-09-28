488a757cc5627d8ddd13bf8c6eeda2b21b86bb8a
Felipe <felipemenezesdantas@gmail.com>
2021-09-28 16:27:30 -0300

    added: endpoint para geração de token

---
+++ API/API/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API.Infrastructure;
+using API.Models;
+using Microsoft.AspNetCore.Authorization;
+using API.Services;
+using System;
+
+namespace API.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public ActionResult<dynamic> Authenticate([FromBody] Acesso login)
+        {
+            var id = _context.Acesso.Where(u => u.Login == login.Login && u.Password == login.Password).SingleOrDefault();
+
+            var user = _context.Acesso.Find(id.Id);
+
+            if (user == null)
+            {
+                return NotFound(new { message = "Usuário ou senha não encontrado" });
+            }
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+    }
+}
