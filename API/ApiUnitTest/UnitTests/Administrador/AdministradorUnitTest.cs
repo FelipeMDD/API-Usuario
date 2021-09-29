a8704970060e0c4ffa03aa7fc4fa66bc0ab266b9
Felipe <felipemenezesdantas@gmail.com>
2021-09-29 13:13:58 -0300

    updated: adicionado projeto para unit test

---
+++ API/ApiUnitTest/UnitTests/Administrador/AdministradorUnitTest.cs
@@ -0,0 +1,107 @@
+﻿using API.Features.Administradores;
+using API.Infrastructure;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace ApiUnitTest.UnitTests.Administrador
+{
+    public class AdministradorUnitTest
+    {
+        [Test]
+        public void ListarUsuarios_RetornaOk()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "ListarUsuarios_RetornaOk")
+                .Options;
+
+            using (var context = new Context(options))
+            {
+                var acesso = new Acesso
+                {
+                    Id = 1,
+                    Nome = "Teste",
+                    Login = "Teste",
+                    Password = "123456",
+                    Email = "teste@gmail.com",
+                    Role = "administrador"
+                };
+
+                context.Acesso.Add(acesso);
+                context.SaveChanges();
+
+                var query = new ListarUsuarios.Query { };
+                var handler = new ListarUsuarios.QueryHandler(context);
+
+                var result = handler.Handle(query);
+
+                Assert.AreEqual(result.Count, 1);
+            }
+        }
+
+        [Test]
+        public void ListarUsuarios_RetornaErr()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "ListarUsuarios_RetornaErr")
+                .Options;
+
+            using (var context = new Context(options))
+            {
+                var acesso = new Acesso
+                {
+                    Id = 1,
+                    Nome = "Teste",
+                    Login = "Teste",
+                    Password = "123456",
+                    Email = "teste@gmail.com",
+                    Role = "administrador"
+                };
+
+                var query = new ListarUsuarios.Query { };
+                var handler = new ListarUsuarios.QueryHandler(context);
+
+                var result = handler.Handle(query);
+
+                Assert.AreEqual(result.Count, 0);
+            }
+        }
+
+        [Test]
+        public void AdicionarUsuario_RetornaOk()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "AdicionarUsuario_RetornaOk")
+                .Options;
+
+            using (var context = new Context(options))
+            {
+                var acesso = new Acesso
+                {
+                    Id = 1,
+                    Nome = "Teste",
+                    Login = "Teste",
+                    Password = "123456",
+                    Email = "teste@gmail.com",
+                    Role = "administrador"
+                };
+
+                context.Acesso.Add(acesso);
+                context.SaveChanges();
+
+                var query = new AdicionarUsuario.Command {
+                    nome = "Teste",
+                    login = "Teste",
+                    password = "123456",
+                    email = "teste@gmail.com",
+                    role = "administrador"
+                };
+                var handler = new AdicionarUsuario.CommandHandler(context);
+
+                var result = handler.Handle(query);
+
+                Assert.IsTrue(result.IsOk);
+            }
+        }
+    }
+}
---
+++ API/ApiUnitTest/UnitTests/Shared/SharedUnitTest.cs
@@ -0,0 +1,105 @@
+﻿using API.Features.Shared;
+using API.Infrastructure;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace ApiUnitTest.UnitTests.Shared
+{
+    public class SharedUnitTest
+    {
+        [Test]
+        public void VerificarCadastro_RetornaOk()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "VerificarCadastro_RetornaOk")
+                .Options;
+
+            using (var context = new Context(options))
+            {
+                var acesso = new Acesso
+                {
+                    Id = 1,
+                    Nome = "Teste",
+                    Login = "Teste",
+                    Password = "123456",
+                    Email = "teste@gmail.com",
+                    Role = "administrador"
+                };
+
+                context.Acesso.Add(acesso);
+                context.SaveChanges();
+
+                var query = new VerificarCadastro.Query { id = 1  };
+                var handler = new VerificarCadastro.QueryHandler(context);
+
+                var result = handler.Handle(query);
+
+                Assert.IsTrue(result.IsSome);
+            }
+        }
+
+        [Test]
+        public void AlterarSenha_RetornaOk()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "AlterarSenha_RetornaOk")
+                .Options;
+
+            using (var context = new Context(options))
+            {
+                var acesso = new Acesso
+                {
+                    Id = 1,
+                    Nome = "Teste",
+                    Login = "Teste",
+                    Password = "123456",
+                    Email = "teste@gmail.com",
+                    Role = "administrador"
+                };
+
+                context.Acesso.Add(acesso);
+                context.SaveChanges();
+
+                var query = new AlterarSenha.Command { id = 1, password = "1234567" };
+                var handler = new AlterarSenha.CommandHandler (context);
+
+                var result = handler.Handle(query);
+
+                Assert.IsTrue(result.IsOk);
+                Assert.AreEqual("1234567", context.Acesso.Find(1).Password);
+            }
+        }
+
+        [Test]
+        public void AlterarSenha_RetornaErr()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "AlterarSenha_RetornaErr")
+                .Options;
+
+            using (var context = new Context(options))
+            {
+                var acesso = new Acesso
+                {
+                    Id = 1,
+                    Nome = "Teste",
+                    Login = "Teste",
+                    Password = "123456",
+                    Email = "teste@gmail.com",
+                    Role = "administrador"
+                };
+
+                context.Acesso.Add(acesso);
+                context.SaveChanges();
+
+                var query = new AlterarSenha.Command { id = 2, password = "123456" };
+                var handler = new AlterarSenha.CommandHandler(context);
+
+                var result = handler.Handle(query);
+
+                Assert.IsTrue(result.IsErr);
+            }
+        }
+    }
+}
